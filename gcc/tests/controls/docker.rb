353765cb0eeb5e43632cc8af07970e70e61e85bc
nagatax <4foview@gmail.com>
2019-01-25 00:24:21 +0900

    試験項目の変更
    試験項目を変更する。

--- gcc/tests/controls/docker.rb
+++ gcc/tests/controls/docker.rb
@@ -8,20 +8,20 @@ control 'docker' do
   desc 'An optional description...'
 
   # docker
-  docker_image('nagatax/gcc:latest') do
+  docker_image('nagatax/gcc:develop') do
     it { should exist }
-    its('repo') { should eq 'docker.io/nagatax/gcc' }
-    its('tag') { should eq 'latest' }
+    its('repo') { should eq 'nagatax/gcc' }
+    its('tag')  { should eq 'develop' }
   end
 
   describe docker_container(name: 'gcc') do
     it { should exist }
     it { should be_running }
-    #its('id') { should_not eq '' }
-    its('image') { should eq 'docker.io/nagatax/gcc' }
-    its('repo') { should eq 'docker.io/nagatax/gcc' }
-    its('tag') { should eq 'latest' }
-    #its('ports') { should eq [] }
+    #its('id')     { should_not eq '' }
+    its('image')   { should eq 'nagatax/gcc' }
+    its('repo')    { should eq 'nagatax/gcc' }
+    its('tag')     { should eq 'develop' }
+    #its('ports')  { should eq [] }
     its('command') { should eq '/bin/bash' }
   end
 
