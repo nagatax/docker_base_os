78703c43abc87df7fa9f030b5a8d137487fcc39b
nagatax <4foview@gmail.com>
2019-01-25 22:43:08 +0900

    リポジトリ名の変更
    リポジトリ名の先頭にある"index."を取り除く。

--- gcc/tests/controls/docker.rb
+++ gcc/tests/controls/docker.rb
@@ -8,7 +8,7 @@ control 'docker' do
   desc 'An optional description...'
 
   # docker
-  describe docker_image(ENV['IMAGE_NAME']) do
+  describe docker_image(ENV['IMAGE_NAME'].gsub(/index\./, "")) do
     it { should exist }
     its('repo') { should eq ENV['DOCKER_REPO'] }
     its('tag')  { should eq nil }
@@ -18,8 +18,8 @@ control 'docker' do
     it { should exist }
     it { should be_running }
     #its('id')     { should_not eq '' }
-    its('image')   { should eq ENV['DOCKER_REPO'] }
-    its('repo')    { should eq ENV['DOCKER_REPO'] }
+    its('image')   { should eq ENV['DOCKER_REPO'].gsub(/index\./, "") }
+    its('repo')    { should eq ENV['DOCKER_REPO'].gsub(/index\./, "") }
     its('tag')     { should eq nil }
     #its('ports')  { should eq [] }
     its('command') { should eq '/bin/bash' }
