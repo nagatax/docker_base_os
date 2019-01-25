85bff3dc38838a2e10f8a0e5b935093109766841
nagatax <4foview@gmail.com>
2019-01-25 23:05:20 +0900

    controlsの変更
    試験項目を変更する。

--- gcc/tests/controls/docker.rb
+++ gcc/tests/controls/docker.rb
@@ -4,24 +4,17 @@ title 'docker section'
 
 control 'docker' do
   impact 0.7
-  title 'Install docker package'
-  desc 'An optional description...'
-
-  # docker
-  describe docker_image(ENV['IMAGE_NAME'].gsub(/index\./, "")) do
-    it { should exist }
-    its('repo') { should eq ENV['DOCKER_REPO'] }
-    its('tag')  { should eq nil }
-  end
+  title 'Check installed gcc package'
+  desc 'Check installed gcc package'
 
   describe docker_container(name: 'sut') do
     it { should exist }
     it { should be_running }
-    its('id')     { should_not eq '' }
-    its('image')   { should eq ENV['DOCKER_REPO'].gsub(/index\./, "") }
-    its('repo')    { should eq ENV['DOCKER_REPO'].gsub(/index\./, "") }
-    its('tag')     { should eq nil }
-    its('ports')  { should eq [] }
+    its('id')      { should_not eq '' }
+    its('image')   { should eq ENV['IMAGE_NAME'] }
+    its('repo')    { should eq ENV['DOCKER_REPO'] }
+    its('tag')     { should eq ENV['CACHE_TAG'] }
+    #its('ports')   { should eq [] }
     its('command') { should eq '/bin/bash' }
   end
 
