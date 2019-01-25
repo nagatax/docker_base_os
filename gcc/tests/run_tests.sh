4fb713d3d18aebd20174836f6e0932b65b05d644
nagatax <4foview@gmail.com>
2019-01-25 19:21:01 +0900

    環境変数の表示
    docker hub内で使用できる環境変数の値を表示する。

--- gcc/tests/run_tests.sh
+++ gcc/tests/run_tests.sh
@@ -7,6 +7,14 @@ curl "https://omnitruck.chef.io/install.sh" | bash -s -- -P inspec
 # docker run
 docker run -it -d --name sut docker.io/nagatax/gcc
 
+echo "SOURCE_BRANCH:${SOURCE_BRANCH}"
+echo "SOURCE_COMMIT:${SOURCE_COMMIT}"
+echo "COMMIT_MSG:${COMMIT_MSG}"
+echo "DOCKER_REPO:${DOCKER_REPO}"
+echo "DOCKERFILE_PATH:${DOCKERFILE_PATH}"
+echo "CACHE_TAG:${CACHE_TAG}"
+echo "IMAGE_NAME:${IMAGE_NAME}"
+
 # execute inspec
 inspec exec . --controls=docker
 inspec exec . -t docker://sut --controls=gcc
