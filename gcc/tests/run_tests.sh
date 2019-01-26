0f23cc54238667f5933537de9cb5c237c157bdf4
nagatax <4foview@gmail.com>
2019-01-26 12:36:44 +0900

    dockerコマンドの変更
    dockerの新コマンドを使用する。

--- gcc/tests/run_tests.sh
+++ gcc/tests/run_tests.sh
@@ -4,12 +4,15 @@ set -eu
 # Install inspec
 curl "https://omnitruck.chef.io/install.sh" | bash -s -- -P inspec
 
-# docker run
-docker run -it -d --name sut "${IMAGE_NAME}"
+# run docker container (create & start)
+docker container run -it -d --name sut "${IMAGE_NAME}"
 
 # execute inspec
 inspec exec . --controls=docker
 inspec exec . -t docker://sut --controls=gcc
 
-# docker container
-docker rmi sut
+# stop docker container
+docker container stop sut
+
+# remove docker container
+docker container rm sut
