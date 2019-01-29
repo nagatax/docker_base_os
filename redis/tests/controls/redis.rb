136c2a7254338ee40ac117ae7fa6df4efd35fc41
nagatax <4foview@gmail.com>
2019-01-29 21:19:38 +0900

    単体試験の追加
    単体試験を追加する。

---
+++ redis/tests/controls/redis.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+title 'redis section'
+
+control 'redis' do
+  impact 0.7
+  title 'Check redis package'
+  desc 'Check redis package'
+
+  # バイナリファイルの存在確認
+  describe file('/root/.bashrc') do
+    it { should be_file  }
+  end
+  describe file('/usr/local/redis') do
+    it { should be_directory  }
+  end
+  describe file('/usr/local/redis/bin/redis-server') do
+    it { should be_file  }
+  end
+
+  # バージョンの確認
+  describe command('. ~/.bashrc && redis -v') do
+    its(:stderr) { should match /5\.0\.3/ }
+  end
+end
