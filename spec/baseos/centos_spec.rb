e37394de629a15b2c21d61a81786a7216f6b0c2d
nagatax <4foview@gmail.com>
2019-09-23 10:47:35 +0900

    試験対象のイメージ名の変更
    試験対象のイメージ名を変更する。

--- spec/baseos/centos_spec.rb
+++ spec/baseos/centos_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 set :backend, :docker
-set :docker_image, 'nagatax/base_os:centos_7'
+set :docker_image, "nagatax/base_os:centos_7"
 
 # バイナリファイルの存在確認
 describe file('/root/.bashrc') do
--- spec/baseos/ubuntu_spec.rb
+++ spec/baseos/ubuntu_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 set :backend, :docker
-set :docker_image, 'nagatax/base_os:ubuntu_bionic'
+set :docker_image, "nagatax/base_os:ubuntu_bionic"
 
 # バイナリファイルの存在確認
 describe file('/root/.bashrc') do
