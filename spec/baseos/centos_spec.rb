e5f2ba50277abb6920398dca60b119defe69af0b
nagatax <4foview@gmail.com>
2019-09-23 12:19:16 +0900

    試験項目の追加
    OSのバージョンを確認する試験を追加する。

--- spec/baseos/centos_spec.rb
+++ spec/baseos/centos_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
 set :backend, :docker
-set :docker_image, "nagatax/base_os:centos_7"
+set :docker_image, ENV['DOCKER_USER'] + "/base_os:centos_7"
 
 # バイナリファイルの存在確認
 describe file('/root/.bashrc') do
   it { should be_file  }
+end
+
+# OSのバージョン確認
+describe command('cat /etc/redhat-release') do
+  its(:stdout) { should match /CentOS Linux release 7/ }
 end
