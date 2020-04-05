22e9f66c5600ba568510659af2e5c9f8e4358fd7
nagatax <4foview@gmail.com>
2020-04-05 17:23:05 +0900

    [apache]機能別にファイルを分け、apacheとopensslのバージョンを最新版にする

---
+++ apache/hooks/env.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+##################################################
+# 環境変数を定義する
+#
+# Environment Variables
+#  PACKAGE: package name
+#  PACKAGE_VERSION: package version
+#  PACKAGE_LATEST: latest version of package
+#  D_IMAGE: image name of Docker
+#  DH_IMAGE: image name of Docker Hub
+#  DH_LATEST: latest image name of Docker Hub
+##################################################
+
+# Set shell option
+set -eu
+
+# Enable BuildKit
+export DOCKER_BUILDKIT=1
+
+# Installing version
+export PACKAGE=httpd
+export PACKAGE_VERSION=2.4.43
+export PACKAGE_LATEST=2.4.43
+export PACKAGE_SHA256="2624e92d89b20483caeffe514a7c7ba93ab13b650295ae330f01c35d5b50d87f"
+export D_IMAGE=${PACKAGE}:${PACKAGE_VERSION}
