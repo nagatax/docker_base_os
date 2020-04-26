fc914fb8a8aaaa0d614dae1bd41f3229cacede2b
nagatax <4foview@gmail.com>
2020-04-26 10:15:18 +0900

    [baseos]ビルドとデプロイスクリプトをスクリプトファイルにまとめる

---
+++ baseos/hooks/env.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+##################################################
+# 環境変数を定義する
+#
+# Environment Variables
+#  PACKAGE: package name
+#  PACKAGE_VERSION: package version
+#  PACKAGE_LATEST: latest version of package
+#  D_IMAGE: image name of Docker
+#  DH_IMAGE: image name of Docker Hub
+#  DH_LATEST: latest image name of Docker Hub
+##################################################
+
+# Set shell option
+set -eu
+
+# Enable BuildKit
+export DOCKER_BUILDKIT=1
+
+# Installing version
+export PACKAGE=centos
+export PACKAGE_VERSION=7
+export PACKAGE_LATEST=7
+export PACKAGE_SHA256=""
+export D_IMAGE=${PACKAGE}:${PACKAGE_VERSION}
