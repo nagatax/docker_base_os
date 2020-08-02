45d9b7c0cb1e64421018245d60bccbf377ffabb4
nagatax <4foview@gmail.com>
2020-08-02 23:21:28 +0900

    [apache]テストスクリプトの追加

---
+++ apache/tests/controls/docker.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+title 'docker section'
+
+control 'docker' do
+  impact 0.7
+  title 'Check docker container'
+  desc 'Check docker container'
+
+  describe docker_container(name: 'sut') do
+    it { should exist }
+    it { should be_running }
+    its('id')      { should_not eq '' }
+    its('image')   { should eq ENV['D_IMAGE'] }
+    its('repo')    { should eq ENV['PACKAGE'] }
+    its('tag')     { should eq ENV['PACKAGE_VERSION'] }
+    #its('ports')   { should eq [] }
+    its('command') { should eq '/usr/local/httpd/bin/apachectl -D FOREGROUND' }
+  end
+
+end
---
+++ apache/tests/run_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu
+
+type "inspec" > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    # Install inspec
+    curl "https://omnitruck.chef.io/install.sh" | bash -s -- -P inspec
+fi
+
+# run docker container (create & start)
+docker container run -it -d --name sut "${D_IMAGE}"
+
+# execute inspec
+inspec exec ./apache/tests --controls=docker --chef-license=accept-silent
+inspec exec ./apache/tests -t docker://sut --controls=apache
+
+# stop docker container
+docker container stop sut
+
+# remove docker container
+docker container rm sut
