9e73e45a260621c322f4105d08114d9ba32cf373
nagatax <4foview@gmail.com>
2020-08-03 21:59:38 +0900

    [gcc]shellの変更とスクリプトの変更

--- gcc/tests/run_tests.sh
+++ gcc/tests/run_tests.sh
@@ -1,18 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 set -eu
-
-type "inspec" > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    # Install inspec
-    curl "https://omnitruck.chef.io/install.sh" | bash -s -- -P inspec
-fi
 
 # run docker container (create & start)
 docker container run -it -d --name sut "${D_IMAGE}"
 
 # execute inspec
-inspec exec ./gcc/tests --controls=docker --chef-license=accept-silent
-inspec exec ./gcc/tests -t docker://sut --controls=gcc
+bundle exec inspec exec ./gcc/tests --controls=docker --chef-license=accept-silent
+bundle exec inspec exec ./gcc/tests -t docker://sut --controls=gcc
 
 # stop docker container
 docker container stop sut
