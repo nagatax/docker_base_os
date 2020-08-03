77150a427f40602c60e4406355fdcbd9384a5ecc
nagatax <4foview@gmail.com>
2020-08-03 22:21:55 +0900

    [nginx]inspec設定フォルダパスの変更

--- nginx/tests/run_tests.sh
+++ nginx/tests/run_tests.sh
@@ -5,8 +5,8 @@ set -eu
 docker container run -it -d --name sut "${D_IMAGE}"
 
 # execute inspec
-bundle exec inspec exec . --controls=docker --chef-license=accept-silent
-bundle exec inspec exec . -t docker://sut --controls=nginx
+bundle exec inspec exec ./nginx/tests --controls=docker --chef-license=accept-silent
+bundle exec inspec exec ./nginx/tests -t docker://sut --controls=nginx
 
 # stop docker container
 docker container stop sut
