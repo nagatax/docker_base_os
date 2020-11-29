31dbc8809c18906511f774fb946b8a0a8ee30848
nagatax <4foview@gmail.com>
2020-11-29 14:53:22 +0900

    [apache]バージョン番号を定義済みの環境変数に置き換える

--- apache/tests/controls/apache.rb
+++ apache/tests/controls/apache.rb
@@ -20,6 +20,6 @@ control 'apache' do
 
   # バージョンの確認
   describe command('. ~/.bashrc && httpd -v') do
-    its(:stdout) { should match /2\.4\.46/   }
+    its(:stdout) { should match /#{ENV['PACKAGE_VERSION']}/ }
   end
 end
